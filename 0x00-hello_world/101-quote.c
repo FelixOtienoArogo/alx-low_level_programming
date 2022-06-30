cb47f94459fb3e4f3bdf576da1cad51d7a7cddbb
FelixOtienoArogo <felixarogo@gmail.com>
2022-06-30 15:52:12 +0300

    he

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-/* here we begin*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	static const char hello[] = "and that piece of art is \
-useful\" - Dora Korpar, 2015-10-19\n";
+	static const char hello[] =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(hello, sizeof(hello) - 1, 1, stdout);
 	return (1);
