963756c3be40156e54aa2a7014dc33d19c18c7ba
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-05 16:20:15 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_sign - Entry point
+ *@c: First member
+ *
+ *Return: Always 0 (Success)
+ */
+
+int print_sign(int c)
+{
+if (c > 0)
+{
+_putchar('+');
+return (1); }
+if (c == 0)
+{
+_putchar('0');
+return (0); }
+else
+{
+_putchar('-');
+return (-1); }
+}
