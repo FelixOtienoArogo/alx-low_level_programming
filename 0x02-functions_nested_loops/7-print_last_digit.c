01b44c9df9d1120996a19c312af2e212fc0ff519
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-05 17:55:14 +0300

    eh

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  *print_last_digit - Entry point
  *@c: First member
- *@a: Second member
+ *
  *
  *Return: Always 0 (Success)
 */
@@ -13,12 +13,14 @@ int print_last_digit(int c)
 int a;
 if (c >= 0)
 a = c % 10;
-else
+if (c < 0)
 {
 a = 0 - c;
 a = a % 10;
 }
+if ((a >= 0) && (a <= 9))
 _putchar(a + '0');
+else
+_putchar(a + 'A');
 return (a);
-c = 0;
 }
