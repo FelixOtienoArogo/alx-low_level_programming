a192e4caf4feb6a01619ff0eeb1e9c4701938bef
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-06 23:26:27 +0300

    fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,9 @@
  */
 int main(void)
 {
-long int i, c, d, temp;
-c = 1;
-d = 0;
-_putchar(d + '0');
-_putchar(',');
-_putchar(' ');
-for (i = 1; i < 50; i++)
-{
-int x, w, v, u, k, l, n, p, q, r, s, t;
-x = (c / 100000000000) % 10;
-w = (c / 10000000000) % 10;
-v = (c / 1000000000) % 10;
-u = (c / 100000000) % 10;
-k = (c / 10000000) % 10;
+long int  c, l, n, p, q, r, s, t;
+c = 4613732;
+
 l = (c / 1000000) % 10;
 n = (c / 100000) % 10;
 p = (c / 10000) % 10;
@@ -28,18 +17,7 @@ q = (c / 1000) % 10;
 r = (c / 100) % 10;
 s = (c / 10) % 10;
 t = c % 10;
- if ((c <= 4000000) && ((c % 2) == 0))
-{
-if (c > 100000000000)
-_putchar(x + '0');
-if (c > 10000000000)
-_putchar(w + '0');
-if (c > 1000000000)
-_putchar(v + '0');
-if (c > 100000000)
-_putchar(u + '0');
-if (c > 10000000)
-_putchar(k + '0');
+
 if (c > 1000000)
 _putchar(l + '0');
 if (c > 100000)
@@ -53,16 +31,7 @@ _putchar(r + '0');
 if (c > 10)
 _putchar(s + '0');
 _putchar(t + '0');
-}
-if (i != 49)
-{
-_putchar(',');
-_putchar(' ');
-}
-temp = d;
-d = c;
-c = temp + c;
-}
+ 
 _putchar('\n');
 return (0);
 }
