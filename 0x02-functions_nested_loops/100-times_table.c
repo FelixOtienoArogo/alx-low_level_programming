13b02ffc26ff0b112d9ee078fc3783c1dc324971
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-06 19:35:42 +0300

    now

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,10 @@
 
 void print_times_table(int n)
 {
-long int i, j;
-
+long int i, j, a;
+a = n * n;
+if (a > 0)
+{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -57,4 +59,4 @@ _putchar (' ');
 _putchar('\n');
 }
 }
-
+}
