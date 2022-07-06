b7dbc2a2f3a4ed0ea7fa3d784f59cc887df719a3
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-06 20:52:07 +0300

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, n;
+n = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+n = n + i;
+}
+}
+_putchar((n % 10) + '0');
+return (0);
+}
