d23d5fbcdf07c8c449599a91c6c2d3170c73222c
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-08 18:12:40 +0300

    no

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 
 void print_number(int n)
 {
-int i, j, k, l, m;
+int i, j, k, l, m, o, p, q, r;
 
 if (n < 0)
 {
@@ -22,7 +22,19 @@ j = (n / 10) % 10;
 k = (n / 100) % 10;
 l = (n / 1000) % 10;
 m = (n / 10000) % 10;
+o = (n / 100000) % 10;
+p = (n / 1000000) % 10;
+q = (n / 10000000) % 10;
+r = (n / 100000000) % 10;
 
+if (n > 100000000)
+_putchar (r + '0');
+if (n > 10000000)
+_putchar (q + '0');
+if (n > 1000000)
+_putchar (p + '0');
+if (n > 100000)
+_putchar (o + '0');
 if (n > 10000)
 _putchar (m + '0');
 if (n > 1000)
