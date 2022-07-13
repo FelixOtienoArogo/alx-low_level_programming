ecaf8557ceb6b5f088b8e1fc8ad4f14abb7ba8c5
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-13 13:18:00 +0300

    cp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 
