5b82e03204b6261e1554347c8958dfda6ec07e93
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-13 14:15:37 +0300

    betty

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * _strcmp - this function compares strings pointed to by s1 and s2
+ * @s1: first pointer
+ * @s2: second pointer
+ *
+ * Return: comparison.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i, j;
+j = strlen(s1) + 1;
+i = 0;
+for (; i < j;)
+{
+if(*(s1 + i) != *(s2 + i))
+return (*(s1 + i) - *(s2 + i));
+else
+return (0);
+i++;
+}
+ return (0);
+}
