25e0aaffe9d89c6e57b171d791d2057cf8015899
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-13 12:56:17 +0300

    cat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ void rev_string(char *s);
 void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
