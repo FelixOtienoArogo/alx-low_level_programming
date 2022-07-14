36a69cc6d87609cc01cae10d23e825b99e8b57de
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-14 22:36:03 +0300

    me

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,18 @@
  */
 char *rot13(char *str)
 {
-int k, l;
+int k, l, m, n;
+char rot1[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot2[53] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 l = strlen(str);
-for (k = 0; k <= l; k++)
+n = strlen(rot1);
+for (k = 0; k < l; k++)
 {
-if ((*(str + k) >= 'a' && *(str + k) < 'n') || (*(str + k) >= 'A' && *(str + k) < 'N'))
-*(str + k) = *(str + k) + 13;
-else if ((*(str + k) >= 'o' && *(str + k) <= 'z') || (*(str + k) >= 'O' && *(str \
-+ k) <= 'Z'))
-*(str + k) = *(str + k) - 13;
+for (m = 0; m < n; m++)
+{
+if (*(str + k) == *(rot1 + m))
+*(str + k) = *(rot2 + m);
+}
 }
 return (str);
 }
