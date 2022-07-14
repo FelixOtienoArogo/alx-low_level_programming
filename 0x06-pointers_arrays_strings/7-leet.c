b2cb91d3a8b55c04316f523bd1d50f06ab6342dd
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-14 20:47:59 +0300

    he

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,13 +16,13 @@ for (k = 0; k <= l; k++)
 {
 if (*(str + k) == 'a' || *(str + k) == 'A')
 *(str + k) = '4';
-else if (*(str + k) == 'e' || *(str + k) == 'E')
+while  (*(str + k) == 'e' || *(str + k) == 'E')
 *(str + k) = '3';
-else if (*(str + k) == 'o' || *(str + k) == 'O')
+while (*(str + k) == 'o' || *(str + k) == 'O')
 *(str + k) = '0';
-else if (*(str + k) == 't' || *(str + k) == 'T')
+while (*(str + k) == 't' || *(str + k) == 'T')
 *(str + k) = '7';
-else if (*(str + k) == 'l' || *(str + k) == 'L')
+while (*(str + k) == 'l' || *(str + k) == 'L')
 *(str + k) = '1';
 }
 return (str);
