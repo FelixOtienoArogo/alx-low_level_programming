81b00aa8676cd0ae563386cd3fcb55de33903d40
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-15 12:27:50 +0300

    m

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
-j = n;
-i = 0;
-for (; i < j;)
+int i;
+for (i = 0; i < n && *(src + i) != '\0'; i++)
 {
 *(dest + i) = *(src + i);
-i++;
+}
+for (; i < n; i++)
+{
+*(dest + i) = '\0';
 }
 return (dest);
 }
--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,18 +1,33 @@
-#include "main.h"
 #include <stdio.h>
 
+char *_strncpy(char *, char *, int);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	char s1[98];
+	char *p;
+	int i;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+	for (i = 0; i < 98 - 1; i++)
+		s1[i] = '-';
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	p = _strncpy(s1, "", 4);
+	printf("%s\n", s1);
+	printf("%s\n", p);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+			printf(" ");
+		if (!(i % 10) && i)
+			printf("\n");
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
