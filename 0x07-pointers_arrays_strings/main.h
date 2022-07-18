a9670710c944a278c96fcd1b8b3563ab2f803a9c
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-18 16:50:31 +0300

    mem

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 
 #endif /* MAIN_H */
