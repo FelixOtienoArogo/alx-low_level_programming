bcd0b72629fb870f72c52a58a000f9a188f5733c
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-19 20:16:29 +0300

    diagsum

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 int _atoi(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
-char *rot13(char *);
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
