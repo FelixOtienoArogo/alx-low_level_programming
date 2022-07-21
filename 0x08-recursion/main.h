acd28d9d83de6c06c16b7809805081dd485c49b3
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-21 11:40:25 +0300

    rev

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 
 #endif /* MAIN_H */
