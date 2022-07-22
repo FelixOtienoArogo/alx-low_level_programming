0781467f0fefbd2c11c573c84927d41b203992f7
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-22 12:17:52 +0300

    he

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _atoi(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
-void print_chessboard(char (*a)[8]);
+int is_prime_number(int n);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
 int factorial(int n);
