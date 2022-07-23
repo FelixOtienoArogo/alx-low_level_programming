5dcec6bb2fcc6280d923db58f3677db95a368006
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-23 16:43:54 +0300

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *@argc: first member
+ *@argv: second argument
+ *
+ *Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
