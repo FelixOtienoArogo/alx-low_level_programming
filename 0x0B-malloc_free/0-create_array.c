13bf45ae58f6ae911b6474243c044c38ee09b99f
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-25 15:39:13 +0300

    arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 char *arr;
 unsigned int i;
 arr = malloc(sizeof(c) * size);
+if (arr == NULL)
+return (0);
+
 if (size == 0)
 return (0);
 for (i = 0; i < size; i++)
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -20,7 +20,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 
 #endif /* MAIN_H */
