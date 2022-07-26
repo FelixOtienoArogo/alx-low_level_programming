0ea6c34d3dab0cee493722876563825d7dfb0005
FelixOtienoArogo <felixarogo@gmail.com>
2022-07-26 22:44:38 +0300

    tostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,30 +3,30 @@
 #include<string.h>
 
 /**
- * *str_concat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
+ * *argstostr - concatenates arguments to string
+ * @ac: the number of arguments
+ * @av: the argv
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *str_concat(char *s1, char *s2)
+char *argstostr(int ac, char **av)
 {
 char *arr;
-int i, size1, size2;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-size1 = strlen(s1);
-size2 = strlen(s2);
-arr = malloc(sizeof(char) * (size1 + size2) +1);
+int i;
+if (ac == 0)
+return (NULL);
+if (av == NULL)
+return (NULL);
+
+arr = malloc(sizeof(*av) * ac + 1);
 if (arr == NULL)
 return (NULL);
 
-for (i = 0; i <= size1; i++)
-arr[i] = s1[i];
-for (i = 0; i <= size2; i++)
-arr[i + size1] = s2[i];
+for (i = 0; i < ac; i += 1)
+{
+strcat(arr, av[i]);
+strcat(arr, "\n");
+}
 return (arr);
 }
