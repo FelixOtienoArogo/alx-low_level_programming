bd1ef967f84251ba2c5ff705736132a307b15c51
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-02 23:47:37 +0300

    god

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,21 @@
  */
 void print_dog(struct dog *d)
 {
+if (d)
+{
+if (d->name)
 printf("Name: %s\n", d->name);
+else if (!d->name)
+printf("Name: nil\n");
+
+if (d->age)
 printf("Age: %f\n", d->age);
+else if (!d->age)
+printf("Age: nil\n");
+
+if (d->owner)
 printf("Owner: %s\n", d->owner);
+else if (!d->owner)
+printf("Owner: nil\n");
+}
 }
