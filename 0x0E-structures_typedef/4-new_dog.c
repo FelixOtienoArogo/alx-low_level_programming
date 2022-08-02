162a7f903d2946f9b1aeb03cddedd132c4dbbaf8
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-03 00:37:44 +0300

    free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *new;
 if (name == NULL)
 return (NULL);
-new = malloc(sizeof(dog_t));
+new = malloc(sizeof(dog_t) + 11);
 if (new == NULL)
 {
 free(name);
