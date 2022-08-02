05687b58a62a76942f651e740ac8cc747972d91f
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-02 23:50:34 +0300

    hi

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,16 +14,16 @@ if (d)
 if (d->name)
 printf("Name: %s\n", d->name);
 else if (!d->name)
-printf("Name: nil\n");
+printf("Name: (nil)\n");
 
 if (d->age)
 printf("Age: %f\n", d->age);
 else if (!d->age)
-printf("Age: nil\n");
+printf("Age: (nil)\n");
 
 if (d->owner)
 printf("Owner: %s\n", d->owner);
 else if (!d->owner)
-printf("Owner: nil\n");
+printf("Owner: (nil)\n");
 }
 }
