8266e7cfdba21c32d22719a3ea04cf8b787503df
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-05 13:29:02 +0300

    hi

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 /**
  *main - to test the code
  *
@@ -17,10 +18,15 @@ if (argc != 4)
 printf("Error\n");
 exit(98);
 }
-if (argv[2] == NULL)
+if (argv[2] == NULL || strlen(argv[2]) != 1)
 {
 printf("Error\n");
 exit(98);
+}
+if (get_op_func(argv[2]) == NULL || argv[2][1] == '0')
+{
+printf("Error\n");
+exit(99);
 }
 if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 {
