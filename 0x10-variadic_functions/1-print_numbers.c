4b48a4dd5d46da02d4de957ca5f916743565f7ee
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-05 15:06:34 +0300

    print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ va_start(ap, n);
 for (i = 0; i < (n); i++)
 {
 printf("%d", va_arg(ap, int));
-if (separator != NULL)
+if (separator != NULL && i != n - 1)
 printf("%s", separator);
 }
 va_end(ap);
