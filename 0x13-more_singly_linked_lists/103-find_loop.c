bfb14c287124f36e6e8adb41ef0379748a5a294e
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-16 02:49:19 +0300

    try

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *find_listint_loop - find the loop in a linked list
+ *
+ *@head: the head of the list
+ *
+ *Return: the address of the node where the loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+
+}
