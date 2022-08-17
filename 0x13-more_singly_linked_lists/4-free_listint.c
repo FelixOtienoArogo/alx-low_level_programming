6acf195bfe68d7f142993aaceac17f91a3a8bea8
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-17 10:45:24 +0300

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,26 +1,59 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- *free_listint - a function to free a list
+ * _create_list - Create a list
  *
- *@head: the head of the list
+ * @n: Number of elements
  *
- *Return: nothing
+ * Return: A pointer to the first element of the created list
  */
-void free_listint(listint_t *head)
+listint_t *_create_list(unsigned int n, ...)
 {
-listint_t *cur;
-if (head != NULL)
-{
-cur = head;
-while (cur->next != NULL)
-{
-cur = cur->next;
-free(cur);
+	va_list args;
+	listint_t *list;
+	listint_t *tmp;
+	listint_t *prev;
+	unsigned int i;
+	int nb;
+
+	va_start(args, n);
+	prev = tmp = list = NULL;
+	i = 0;
+	while (i < n)
+	{
+		nb = va_arg(args, int);
+		tmp = malloc(sizeof(*tmp));
+		if (!tmp)
+			return (NULL);
+		tmp->n = nb;
+		tmp->next = NULL;
+		if (!list)
+			list = tmp;
+		if (prev)
+			prev->next = tmp;
+		prev = tmp;
+		++i;
+	}
+	va_end(args);
+	return (list);
 }
-free(head);
-}
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	size_t n;
+
+	head = _create_list(1, 9);
+	n = print_listint(head);
+	printf("-> %lu elements\n", n);
+	free_listint(head);
+	return (0);
 }
