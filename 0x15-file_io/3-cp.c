ef354a8fd1d1138f66be7e6da943ffcd7fdb1862
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-23 23:06:58 +0300

    try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,5 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
@@ -9,13 +7,53 @@
 #include <fcntl.h>
 #include "main.h"
 
-/**maini = to copy contetn of a file to another file
+/**
+ *main - to copy contetn of a file to another file
+ *
+ *@argc: the number of arguments
+ *@argv: the argumants
  *
  *Return: 0
  */
 int main(int argc, char **argv)
 {
+char *buf, *NAME_OF_THE_FILE;
+int size, fd, a;
+struct stat sb;
 
-
+if (argc != 3)
+{
+printf("Usage: cp file_from file_to\n");
+exit(97);
+}
+/*read */
+NAME_OF_THE_FILE = argv[1];
+fd = open(argv[1], O_RDWR);
+stat(argv[1], &sb);
+size = 1024;
+buf = malloc(size * 8);
+a = read(fd, buf, size);
+if (a == -1 || fd == -1)
+{
+printf("Error: Can't read from file %s\n", NAME_OF_THE_FILE);
+exit(98);
+}
+close(fd);
+/* WRITE */
+fd = open(argv[2], O_CREAT | O_RDWR, 0600);
+a = write(fd, buf, size);
+if (a == -1 || fd == -1)
+{
+printf("Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+chmod(argv[2], sb.st_mode);
+free(buf);
+a = close(fd);
+if (a == -1)
+{
+printf("Error: Can't close fd %d\n", fd);
+exit(100);
+}
 return (0);
 }
