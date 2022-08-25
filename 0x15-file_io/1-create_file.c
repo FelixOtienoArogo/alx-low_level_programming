cebcb980d3ced4a81a07f9d8b4b063541f67408d
FelixOtienoArogo <felixarogo@gmail.com>
2022-08-25 22:02:59 +0300

    cre

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,10 @@ int create_file(const char *filename, char *text_content)
 int fd;
 size_t cont;
 ssize_t t;
+struct stat sb;
 
 fd = open(filename, O_CREAT | O_RDWR, 0600);
+stat(filename, &sb);
 cont = strlen(text_content);
 if (filename == NULL)
 {
@@ -37,5 +39,6 @@ if (t == -1)
 return (-1);
 }
 close(fd);
+chmod(filename, sb.st_mode);
 return (1);
 }
